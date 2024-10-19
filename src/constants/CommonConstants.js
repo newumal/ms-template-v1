054659c4b5715767540d99987fddbcc0ffe5b3d3
Newumal Weerasinghe <newumaalweerasinghe@gmail.com>
2024-10-19 21:42:50 +0530

    feat: added ms-template-v1

--- src/constants/CommonConstants.js
+++ src/constants/CommonConstants.js
@@ -1,6 +1,6 @@
 export const CONTEXT_KEY = 'core-ms';
 export const CORRELATION_ID_CONTEXT_KEY = 'correlation-id';
 export const AUTHORIZATION_CONTEXT_KEY = 'authorization';
-export const ABC_SRC_HEADER_KEY = 'x-velaris-source';
-export const ABC_CACHE_CONTROL_HEADER_KEY = 'x-velaris-cache-control';
+export const ABC_SRC_HEADER_KEY = 'x-newumal-source';
+export const ABC_CACHE_CONTROL_HEADER_KEY = 'x-newumal-cache-control';
 export const NO_CACHE_OPTION = 'no-cache';
